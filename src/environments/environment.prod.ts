3d2830753e11b42ba90bfd3faa66284f4689eaf9
Mamadou Ngaido <mamadou@Mamadous-MacBook-Pro.local>
2017-12-03 16:12:43 -0500

    new Home page

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB0kbPP5ImyroA1dAyTQc7J5LqgAAk_KYM",
+    authDomain: "annuaire-fe22b.firebaseapp.com",
+    databaseURL: "https://annuaire-fe22b.firebaseio.com",
+    projectId: "annuaire-fe22b",
+    storageBucket: "annuaire-fe22b.appspot.com",
+    messagingSenderId: "265090623240"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB0kbPP5ImyroA1dAyTQc7J5LqgAAk_KYM",
+    authDomain: "annuaire-fe22b.firebaseapp.com",
+    databaseURL: "https://annuaire-fe22b.firebaseio.com",
+    projectId: "annuaire-fe22b",
+    storageBucket: "annuaire-fe22b.appspot.com",
+    messagingSenderId: "265090623240"
+  }
+};
